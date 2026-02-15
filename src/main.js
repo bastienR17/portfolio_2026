5fa577b22ec4d4c7d863b25767f079fc88210d31
bastien <bastien.roc@edu.devinci.fr>
2026-02-15 22:36:02 +0100

    Move CSS to src to bypass path issues

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import i18n from './i18n'
-import './main.css'
+import './assets/Main.css'
 
 
 const app = createApp(App)
