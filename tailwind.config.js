1f7a9d56e99fc335c06c35f9e75d9e14e225b0ed
bastien.roc <bastien.roc@developpement-durable.gouv.fr>
2026-02-26 16:23:47 +0100

    fix js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
+// @ts-nocheck
 /** @type {import('tailwindcss').Config} */
 export default {
+
   darkMode: 'class', 
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: { 
+    extend: {
       colors: {
-        terracotta: '#E2725B',
+        'terracotta': '#E2725B',
         'dark-soft': '#2D2D2D',
-        cream: '#FDFBF7',
-        ochre: '#B45309',
+        'cream': '#FDFBF7',
+        'ochre': '#B45309', 
       },
     },
   },
